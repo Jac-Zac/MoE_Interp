a1a0d1ff3677175b28ecd0a9b6381e501b6fb96c
Jac-Zac <jacopozac@icloud.com>
2026-03-01 09:21:29 +0100

    Updated cineca scripts

---
+++ scripts/cineca/analyze.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=112
+#SBATCH --partition=boost_usr_prod
+#SBATCH -A uTS25_Tornator_0
+#SBATCH -t 00:30:00
+#SBATCH --job-name=analyze_moe_act
+#SBATCH -o analyze.out
+
+source scripts/setup_env.sh
+python main.py pursuit --k 100
--- scripts/cineca/extract.sh
+++ scripts/cineca/extract.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --partition=boost_usr_prod
 #SBATCH -A uTS25_Tornator
-#SBATCH -t 00:20:00
+#SBATCH -t 00:30:00
 ##SBATCH --exclusive
-#SBATCH --job-name=self_attention_bench
+#SBATCH --job-name=extract_moe_act
+#SBATCH -o extract.out
 
 source scripts/setup_env.sh
-
-python main.py
+python main.py encode --n_docs 50000 --batch_size 64
